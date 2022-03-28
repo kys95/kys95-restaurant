9628928c011d4409f8b2375746e97375978a547c
Yunseok Kang <mok02198@gmail.com>
2022-03-28 23:05:33 +0900

    Init: 프로젝트 생성

---
+++ src/test/java/com/kys95/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kys95.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
